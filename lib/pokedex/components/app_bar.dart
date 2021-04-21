ebf5836921170a2fafa0f60a41093b8b5458804d
joaoaugust2019 <jhonaugustjunior@gmail.com>
2021-04-21 16:16:57 -0300

    extract code

---
+++ lib/pokedex/components/app_bar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).padding.top,
+          ),
+          Container(
+            child: Column(
+              children: [
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 18, right: 6),
+                        child: IconButton(
+                          icon: Icon(Icons.menu),
+                          onPressed: () {},
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Text(
+                        'Pokedex',
+                        style: TextStyle(
+                            fontFamily: 'Google',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 28),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            height: 120,
+            // color: Colors.redAccent,
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/pokedex/pages/pokedex.dart
+++ lib/pokedex/pages/pokedex.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learning/consts/consts_app.dart';
+import 'package:flutter_learning/pokedex/components/app_bar.dart';
 
 class Pokedex extends StatelessWidget {
   @override
@@ -6,7 +8,35 @@ class Pokedex extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Pokedex - João Augusto",
-      home: Text('data'),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        clipBehavior: Clip.none,
+        alignment: Alignment.topCenter,
+        children: [
+          Positioned(
+            top: -(240 / 4.5),
+            left: MediaQuery.of(context).size.width - (240 / 1.6),
+            child: Opacity(
+              child: Image.asset(
+                ConstsApp.darkPokeball,
+                height: 240,
+                width: 240,
+              ),
+              opacity: .1,
+            ),
+          ),
+          AppBarWidget(),
+        ],
+      ),
     );
   }
 }
